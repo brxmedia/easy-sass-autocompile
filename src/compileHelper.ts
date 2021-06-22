71a99b45118fb32f9bc7832a0d9791f9009a29a6
brxmedia <brix.pb@googlemail.com>
2021-06-22 13:49:59 +0200

    async sass compile helper

--- src/compileHelper.ts
+++ src/compileHelper.ts
@@ -3,7 +3,10 @@ import * as path from 'path';
 import { fileHelper } from './fileHelper';
 import { helper } from './helper';
 
-
+export interface ICompile {
+    err: boolean,
+    compileErr: boolean
+}
 export class compileHelper {
     private sassBin: string = "";
     private SassCompiler: any = null;
@@ -76,49 +79,102 @@ export class compileHelper {
             let outputPath = path.join(tTargetPath, paths.css);
 
             let compiled = this.compileCss(targetPath, sourceMap, outputPath);
-            if (compiled) {
-                if (minify) {
-                    let outputMinPath = path.join(tTargetPath, paths.min);
 
-                    this.compileCss(targetPath, sourceMap, outputMinPath, "compressed");
+            compiled.then(compiled => {
+                if (!compiled.compileErr) {
+                    if (!compiled.err) {
+                        if (minify) {
+                            let outputMinPath = path.join(tTargetPath, paths.min);
+
+                            let compiledMap = this.compileCss(targetPath, sourceMap, outputMinPath, "compressed");
+                            compiledMap.then(compiledMap => {
+                                if (!compiledMap.err) {
+                                    helper.outputMessage('Successfully compiled', []);
+                                    helper.statusBarUi.success();
+                                }
+                                else {
+                                    helper.outputMessage('Error compiling', [], true, true);
+                                    helper.statusBarUi.error();
+                                }
+                            });
+                        }
+                        else {
+                            helper.outputMessage('Successfully compiled', []);
+                            helper.statusBarUi.success();
+                        }
+                    }
+                    else {
+                        helper.outputMessage('Error compiling', [], true, true);
+                        helper.statusBarUi.error();
+                    }
                 }
-
-                helper.outputMessage('Successfully compiled', []);
-                helper.statusBarUi.success();
-            }
-            else{
-                helper.outputMessage('Error compiling', [], true, true);
-                helper.statusBarUi.error();
-            }
+            });
         });
     }
 
     compileCss(_file: string, _sourceMap: boolean, _outFile: string, _outputStyle: string = "expanded") {
-        try {
-            let result = this.SassCompiler.renderSync({
-                file: _file,
-                sourceMap: _sourceMap,
-                outFile: _outFile,
-                outputStyle: _outputStyle
-            });
+        return new Promise<ICompile>((resolve) => {
+            try {
+                let result = this.SassCompiler.renderSync({
+                    file: _file,
+                    sourceMap: _sourceMap,
+                    outFile: _outFile,
+                    outputStyle: _outputStyle
+                });
 
 
-            let generated = fileHelper.instance.writeFile(_outFile, result.css);
-            if (generated) {
-                if (_sourceMap) {
-                    fileHelper.instance.writeFile(_outFile + ".map", result.map);
-                }
-                return true;
+                let file = fileHelper.instance.writeFile(_outFile, result.css);
+
+                file.then(file => {
+                    if (file.err != null) {
+                        helper.cacheMessage('Compiler Error: ' + file.FilePath.toString());
+                        helper.cacheMessage(' - ' + file.err.toString());
+                        resolve({
+                            err: true,
+                            compileErr: false
+                        });
+                    }
+                    else {
+                        helper.cacheMessage('Successfully compiled: ' + file.FilePath.toString());
+                        
+                        if (_sourceMap) {
+                            let fileMap = fileHelper.instance.writeFile(_outFile + ".map", result.map);
+                            fileMap.then(fileMap => {
+                                if (fileMap.err != null) {
+                                    helper.cacheMessage('Compiler Error: ' + fileMap.FilePath.toString());
+                                    helper.cacheMessage(' - ' + fileMap.err.toString());
+                                    resolve({
+                                        err: true,
+                                        compileErr: false
+                                    });
+                                }
+                                else {
+                                    helper.cacheMessage('Successfully compiled: ' + fileMap.FilePath.toString());
+                                    resolve({
+                                        err: false,
+                                        compileErr: false
+                                    });
+                                }
+                            });
+                        }
+                        resolve({
+                            err: false,
+                            compileErr: false
+                        });
+                    }
+                });
+
+            } catch (error) {
+                helper.systemMessage('Could not compile SASS. ' + _file + ' Check Outputs for more information.', 'error');
+                helper.statusBarUi.error();
+
+                helper.outputMessage('Sass Error', ['Sass syntax error at line ' + error.line + ', column ' + error.column, error.file], true);
+
+                return {
+                    err: true,
+                    sassErr: true
+                };
             }
-            return false;
-
-        } catch (error) {
-            helper.systemMessage('Could not compile SASS. ' + _file + ' Check Outputs for more information.', 'error');
-            helper.statusBarUi.error();
-
-            helper.outputMessage('Sass Error', ['Sass syntax error at line ' + error.line + ', column ' + error.column, error.file], true);
-
-            return false;
-        }
+        });
     }
 }
