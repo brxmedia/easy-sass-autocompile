4e298fb33e332ea320fca7d1a5b3833d9ad3a8fc
brxmedia <brix.pb@googlemail.com>
2021-06-24 17:48:53 +0200

    add statusbar and message helper to helper

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,5 +1,7 @@
 import { fileHelper } from "./fileHelper";
 import { compileHelper } from "./compileHelper";
+import { statusBarUi } from "./satusBarUi";
+import { messageHelper } from "./messageHelper";
 
 export class esac {
     public static get compile(){
@@ -7,5 +9,11 @@ export class esac {
     }
     public static get file(){
         return fileHelper.instance;
+    }
+    public static get satusBar(){
+        return statusBarUi;
+    }
+    public static get message(){
+        return messageHelper;
     }
 }
